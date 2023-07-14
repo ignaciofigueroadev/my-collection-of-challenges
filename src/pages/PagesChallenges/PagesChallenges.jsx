1ab02a2667875f02b964e7907090290c23c9cef4
Ignacio Figueroa <figueroaignaciodev@gmail.com>
2023-07-14 19:35:38 -0300

    feat: code refactored

--- src/pages/PagesChallenges/PagesChallenges.jsx
+++ src/pages/PagesChallenges/PagesChallenges.jsx
@@ -1,7 +1,15 @@
+// Motion import
 import { motion } from "framer-motion";
+
+// Component imports
 import ChallengeCard from "../../components/ChallengeCard/ChallengeCard";
 import Header from "../../components/Header/Header";
+
+// Styles import
 import styles from "./PagesChallenges.module.css";
+
+// Data import
+import data from "./json/pagesChallenges.json";
 
 const PagesChallenges = () => {
   return (
@@ -13,38 +21,17 @@ const PagesChallenges = () => {
     >
       <Header page="/challenges" />
       <section className={styles.pagesChallenges}>
-        <ChallengeCard
-          img="/images/pages-images/sunnysude-agency-landing-page.png"
-          title="Sunnyside agency landing page"
-          description="Challenge of Frontend Mentor"
-          technologies="HTML & SASS | vanilla JS"
-          github="https://github.com/ignaciofigueroadev/sunnyside-agency-landing-page"
-          live="https://ignaciofigueroadev.github.io/sunnyside-agency-landing-page/"
-        />
-        <ChallengeCard
-          img="/images/pages-images/loopstudios-landing-page.png"
-          title="Loopstudios landing page"
-          description="Challenge of Frontend Mentor"
-          technologies="HTML & SASS | vanilla JS"
-          github="https://github.com/ignaciofigueroadev/loopstudios-landing-page/tree/main"
-          live="https://ignaciofigueroadev.github.io/loopstudios-landing-page/"
-        />
-        <ChallengeCard
-          img="/images/pages-images/interior-consultant-challenge.png"
-          title="Interior consultant challenge"
-          description="Challenge of devChallenges"
-          technologies="HTML & SASS | vanilla JS"
-          github="https://github.com/ignaciofigueroadev/interior-consultant-challenge"
-          live="https://ignaciofigueroadev.github.io/interior-consultant-challenge/"
-        />
-        <ChallengeCard
-          img="/images/pages-images/news-homepage-challenge-main.jpg"
-          title="News homepage main challenge"
-          description="Challenge of Frontend Mentor"
-          technologies="Reactjs | SASS"
-          github="https://github.com/ignaciofigueroadev/news-homepage-main"
-          live="https://ignaciofigueroadev.github.io/news-homepage-main/"
-        />
+        {data.pages.map((page) => (
+          <ChallengeCard
+            key={page.id}
+            img={page.img}
+            title={page.title}
+            description={page.description}
+            technologies={page.technologies}
+            github={page.github}
+            link={page.live}
+          />
+        ))}
       </section>
     </motion.div>
   );
